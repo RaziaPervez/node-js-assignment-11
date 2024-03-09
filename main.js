035be779287df8a6b809adfd9684a04e7e19558f
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 10:00:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//project=11
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+var friendName = ["Maheen", "Nadia", "Isha", "Muskan"];
+friendName.forEach(function (friendName) { return console.log(friendName); });
---
+++ main.ts
@@ -0,0 +1,6 @@
+//project=11
+
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let friendName = ["Maheen","Nadia","Isha","Muskan"];
+friendName.forEach(friendName=>console.log(friendName));
